9d52d5e4b469084661d7a1df9ac04bc755ea4371
Thiago Silva <thiago.tlsbomu@gmail.com>
2022-11-26 13:28:00 -0300

    Fixed docker

--- docker/init.sh/init_db.sh
+++ docker/init.sh/init_db.sh
@@ -1,6 +1,6 @@
 echo "** Creating default DB and users"
 
-mysql -u root -p $MYSQL_ROOT_PASSWORD --execute \
+mysql -u root -p$MYSQL_ROOT_PASSWORD --execute \
 "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
